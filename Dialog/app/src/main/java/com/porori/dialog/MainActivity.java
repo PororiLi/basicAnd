433bdef166dd2a7665ecdab2e617efbf72837036
pororili <xiozioll@gmail.com>
2021-04-09 13:49:52 +0900

    Dialog alertDialog example

---
+++ Dialog/app/src/main/java/com/porori/dialog/MainActivity.java
@@ -0,0 +1,56 @@
+package com.porori.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button  btn_dialog;
+    TextView tv_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_dialog = (Button)findViewById(R.id.btn_dialog);
+        tv_result = (TextView)findViewById(R.id.tv_result);
+
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);   //dialog 아이콘 설정
+                ad.setTitle("Title");
+                ad.setMessage("열심히 공부 하고 있나요?");
+
+                EditText et = new EditText(MainActivity.this);
+                ad.setView(et); //dialog에 et를 추가해라.
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String result = et.getText().toString();
+                        tv_result.setText(result);
+                        dialog.dismiss();   //dialog 종료
+                    }
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();  //dialog 화면 보이기
+            }
+        });
+    }
+}
